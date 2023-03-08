1fb5ba09ed4b3383721f939b536458d35b7ededb
ANIL Charif Zoher <98174097+Anilcharif@users.noreply.github.com>
2023-03-09 00:47:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!-- header -->
+<?php
+    session_start();
+    include_once "./src/header.inc.php";
+    include_once "./src/connect_bdd.inc.php";
+?>
+
+
+</head>
+<body>
+    <header>
+      
+ 
+
+    <nav>
+        <img src="./image/mail.png" alt="logo" class="logo" loading="lazy">
+        <h1 class="title">Gmail</h1>
+        <div class="nav-buttons">
+            <a href="#">Pour les Pros</a>
+            <a href="#">Connexion</a>
+            <a href="#">Creer un compte</a>
+        </div>
+    </nav>
+</header>
+
+
+
+    <main>
+
+        <section class="hook-gmail">
+            <h1>Retrouver la fluidité et la simplicité de Gmail sur tous vos appareil </h1>
+            
+            <a href="index2.html" class="nav-buttons-hook">Creer un compte</a>
+                
+        </section>
+
+        <button>
+            <img src="./image/arrow.png" alt="lazy" loading="lazy">
+        </button>
+
+        
+
+
+
+    </main>
+    
+</body>
+</html>
+
